06b28e0023284a4ff68b034218feb0ba34fd1c5f
Jeffrey Stanley <stanley.l.jeff@gmail.com>
2023-09-10 16:05:10 -0400

    Add adjustments to algorithm
    Added some adjustments to the algorithm to help understand the computation that is occurring during the loop.

--- RichestCustomerWealth.py
+++ RichestCustomerWealth.py
@@ -1,7 +1,13 @@
-def maximumWealth(self, accounts):
+def maximumWealth(accounts):
     mostWealth = 0
     wealth = 0
     for i in range(len(accounts)):
         wealth = sum(accounts[i])
+        print("Current customer wealth: " + str(i) + " "+ str(wealth))
         mostWealth = max(mostWealth, wealth)
-    return mostWealth
+    return "Customer index " + str(i) + " has the highest wealth with a value of " + str(mostWealth)
+
+
+accounts = [[9, 5, 3], [3, 4, 5], [9, 6, 8]]
+mostWealth = maximumWealth(accounts)
+print(mostWealth)

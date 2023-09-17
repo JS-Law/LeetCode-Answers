3c5463c751054fa81acd10c2c879e10048a667a4
Jeffrey Stanley <stanley.l.jeff@gmail.com>
2023-09-17 18:47:11 -0400

    Add solution to FindTheDifference

---
+++ FindTheDifference.py
@@ -0,0 +1,57 @@
+def findTheDifference(s, t):
+    s_count = {}
+    t_count = {}
+
+    # Count characters in string s
+    for char in s:
+        s_count[char] = s_count.get(char, 0) + 1
+    print("S COUNT", s_count, "\n")
+
+    # Count characters in string t
+    for char in t:
+        t_count[char] = t_count.get(char, 0) + 1
+    print("T COUNT", t_count)
+
+    # Compare the character counts in both dictionaries
+    for char in t_count:
+        if char not in s_count or s_count[char] != t_count[char]:
+            pr
+            t("The difference is: ", char)
+            return char
+
+
+s = 'ymbgaraibkfmvocpizdydugvalagaivdbfsfbepeyccqfepzvtpyxtbadkhmwmoswrcxnargtlswqemafandgkmydtimuzvjwxvlfwlhvkrgcsit' \
+    'haqlcvrihrwqkpjdhgfgreqoxzfvhjzojhghfwbvpfzectwwhexthbsndovxejsntmjihchaotbgcysfdaojkjldprwyrnischrgmtvjcorypvop' \
+    'fmegizfkvudubnejzfqffvgdoxohuinkyygbdzmshvyqyhsozwvlhevfepdvafgkqpkmcsikfyxczcovrmwqxxbnhfzcjjcpgzjjfateajnnvlbw' \
+    'hyppdleahgaypxidkpwmfqwqyofwdqgxhjaxvyrzupfwesmxbjszolgwqvfiozofncbohduqgiswuiyddmwlwubetyaummenkdfptjczxemryuo' \
+    'trrymrfdxtrebpbjtpnuhsbnovhectpjhfhahbqrfbyxggobsweefcwxpqsspyssrmdhuelkkvyjxswjwofngpwfxvknkjviiavorwyfzlnktmf' \
+    'wxkvwkrwdcxjfzikdyswsuxegmhtnxjraqrdchaauazfhtklxsksbhwgjphgbasfnlwqwukprgvihntsyymdrfovaszjywuqygpvjtvlsvvqbvz' \
+    'smgweiayhlubnbsitvfxawhfmfiatxvqrcwjshvovxknnxnyyfexqycrlyksderlqarqhkxyaqwlwoqcribumrqjtelhwdvaiysgjlvksrfvjlca' \
+    'iwrirtkkxbwgicyhvakxgdjwnwmubkiazdjkfmotglclqndqjxethoutvjchjbkoasnnfbgrnycucfpeovruguzumgmgddqwjgdvaujhyqsqtoex' \
+    'mnfuluaqbxoofvotvfoiexbnprrxptchmlctzgqtkivsilwgwgvpidpvasurraqfkcmxhdapjrlrnkbklwkrvoaziznlpor'
+t = 'qhxepbshlrhoecdaodgpousbzfcqjxulatciapuftffahhlmxbufgjuxstfjvljybfxnenlacmjqoymvamphpxnolwijwcecgwbcjhgdybfffwoy' \
+    'gikvoecdggplfohemfypxfsvdrseyhmvkoovxhdvoavsqqbrsqrkqhbtmgwaurgisloqjixfwfvwtszcxwktkwesaxsmhsvlitegrlzkvfqoiiwxb' \
+    'zskzoewbkxtphapavbyvhzvgrrfriddnsrftfowhdanvhjvurhljmpxvpddxmzfgwwpkjrfgqptrmumoemhfpojnxzwlrxkcafvbhlwrapubhve' \
+    'attfifsmiounhqusvhywnxhwrgamgnesxmzliyzisqrwvkiyderyotxhwspqrrkeczjysfujvovsfcfouykcqyjoobfdgnlswfzjmyucaxuaslz' \
+    'wfnetekymrwbvponiaojdqnbmboldvvitamntwnyaeppjaohwkrisrlrgwcjqqgxeqerjrbapfzurcwxhcwzugcgnirkkrxdthtbmdqgvqxilllr' \
+    'sbwjhwqszrjtzyetwubdrlyakzxcveufvhqugyawvkivwonvmrgnchkzdysngqdibhkyboyftxcvvjoggecjsajbuqkjjxfvynrjsnvtfvgpgvey' \
+    'cxidhhfauvjovmnbqgoxsafknluyimkczykwdgvqwlvvgdmufxdypwnajkncoynqticfetcdafvtqszuwfmrdggifokwmkgzuxnhncmnsstffqp' \
+    'qbplypapctctfhqpihavligbrutxmmygiyaklqtakdidvnvrjfteazeqmbgklrgrorudayokxptswwkcircwuhcavhdparjfkjypkyxhbgwxbk' \
+    'vpvrtzjaetahmxevmkhdfyidhrdeejapfbafwmdqjqszwnwzgclitdhlnkaiyldwkwwzvhyorgbysyjbxsspnjdewjxbhpsvj'
+# result = findTheDifference(s,t)
+# print(result)
+findTheDifference(s, t)
+
+# BRUTE FORCE METHOD THAT WAS ABANDONED
+
+# i = j = 0
+# result = ""
+# while i < len(t):
+#     if j < len(s) and s[j] == t[i]:
+#         i += 1
+#         j += 1
+#     else:
+#         result = t[i]
+#         print(result)
+#         return t[i]
+# Create dictionaries to store character counts
